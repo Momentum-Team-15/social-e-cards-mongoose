d49cd8c38142b98041b60a5114c476a24d83de25
AlexiosIKomnenos <106697127+AlexiosIKomnenos@users.noreply.github.com>
2022-11-16 12:44:57 -0500

    startpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
+class CardList(generics.ListCreateAPIView): 
+    pass
+
+class MyCardList(generics.ListCreateAPIView):
+    pass
+
+class FriendsCardList(generics.ListCreateAPIView):
+    pass
+
