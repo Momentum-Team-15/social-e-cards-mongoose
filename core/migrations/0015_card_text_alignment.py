c2e2c5ba4a2a8c9fd9ddfea6f8ea99b7c5afc28b
Nick Wilcox <Nickwilcox001@gmail.com>
2022-11-30 15:01:44 -0500

    added CORS origin and altered migrations

--- core/migrations/0015_card_text_alignment.py
+++ core/migrations/0015_card_text_alignment.py
@@ -7,6 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('core', '0014_alter_card_color_alter_card_font_color'),
+        ('core', '0015_alter_card_color_alter_card_font_color')
     ]
 
     operations = [
